e255cd290ac26a24013e8f9d35dbc5a82496fb42
Moniem <abdelmoniem.m.a.o@gmail.com>
2025-12-19 07:15:54 +0200

    The End

--- js/Script.js
+++ js/Script.js
@@ -11,9 +11,9 @@ function check_email_password() {
   let e_mail1 = "valt@gmail.com";
   let password1 = "1234";
   let e_mail2 = "mon3em@gmail.com";
-  let password2 = "12345";
+  let password2 = "1234";
   let e_mail3 = "ahmed@gmail.com";
-  let password3 = "12346";
+  let password3 = "1234";
 
   let mal = document.getElementById("e").value;
   let pass = document.getElementById("p").value;
